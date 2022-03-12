3b12f6abea975e00079b64bae9f3876ca384f6e0
abubjazov <abubjazov@mail.ru>
2022-03-12 23:16:00 +0300

    topic: prop types

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,1 +1,7 @@
+import PropTypes from 'prop-types'
+
 export const Counter = ({ value }) => <h1>{`Counter value is: ${value}`}</h1>
+
+Counter.propTypes = {
+	value: PropTypes.number.isRequired,
+}
--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,4 +1,5 @@
 import { Component } from 'react'
+
 import { Counter } from './Counter'
 
 export class CounterButton extends Component {
