35680382fb6e64df87e77ec6afc9b363ddbd4648
abubjazov <abubjazov@mail.ru>
2022-03-12 22:40:23 +0300

    topic: component state

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -5,10 +5,26 @@ export class CounterButton extends Component {
 		counter: 0,
 	}
 
-	handleClick = () => {}
+	handleClick = operation => {
+		if (operation === '+') {
+			this.setState({
+				counter: this.state.counter + 1,
+			})
+		} else {
+			this.setState({
+				counter: this.state.counter - 1,
+			})
+		}
+	}
 
 	render() {
 		const { counter } = this.state
-		return <div>{counter}</div>
+		return (
+			<>
+				<div>{counter}</div>
+				<button onClick={() => this.handleClick('+')}> + </button>
+				<button onClick={() => this.handleClick('-')}> - </button>
+			</>
+		)
 	}
 }
