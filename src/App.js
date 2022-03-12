453ed6d5cd05f43bebee3c5babe762195bc0d9ea
abubjazov <abubjazov@mail.ru>
2022-03-12 21:33:56 +0300

    topic: functional component VS class component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Component } from 'react'
 import './App.css'
 
 export const App = () => {
-	return <div className='App'></div>
+	return <div className='App'>Func App</div>
+}
+
+export class AppClass extends Component {
+	render() {
+		return <div className='App'>Class App</div>
+	}
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { App } from './App'
+import { App, AppClass } from './App'
 
 import './index.css'
 
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
+		<AppClass />
 	</React.StrictMode>,
 	document.getElementById('root')
 )
